f10f633959db5e5a78c9878264cacba89d09f180
Adjalma Aguiar <121559885+Adjalma@users.noreply.github.com>
2023-11-04 14:11:44 -0300

    Add files via upload

---
+++ Bot completo 3 contas.py
@@ -0,0 +1,469 @@
+
+import time
+import pyautogui
+
+pyautogui.PAUSE = 0.5
+
+# Ir para a conta do Gengis
+pyautogui.moveTo (1583,1061)
+pyautogui.mouseDown()
+pyautogui.mouseUp()
+time.sleep (3)
+
+# Ir para a ilha para atacar as aldeias barbaras
+pyautogui.moveTo (1410, 126)
+pyautogui.mouseDown()
+pyautogui.mouseUp()
+time.sleep (3)
+
+while True:
+ pyautogui.moveTo(2341, 566)  # Ir para a ilha e clicar na aldeia 1
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2055, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2194, 594)  # Ir para a ilha e clicar na aldeia 2
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2055, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2246,687)  # Ir para a ilha e clicar na aldeia 3
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2055, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2374,676)  # Ir para a ilha e clicar na aldeia 4
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2055, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2433,719)  # Ir para a ilha e clicar na aldeia 5
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2055, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2516,733)  # Ir para a ilha e clicar na aldeia 6
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2055, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2394,124)  # Clicar para ir para cidade Gengis II
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2321, 193)  # Clicar em Gengis II
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(1478, 145)  # Clicar para mudar de ilha
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2433,489)  # Ir para a ilha e clicar na aldeia 1 da cidade II
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2066, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2671, 316)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2532,535)  # Ir para a ilha e clicar na aldeia 2 da cidade II
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2066, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2671, 316)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2565,644)  # Ir para a ilha e clicar na aldeia 3 da cidade II
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2066, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2671, 316)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2316,667)  # Ir para a ilha e clicar na aldeia 4 da cidade II
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2066, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2671, 316)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2394, 124)  # Clicar para ir para cidade Gengis
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2328, 170)  # Clicar em Gengis
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(1478, 145)  # Clicar para mudar de ilha
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(4)
+
+ pyautogui.moveTo(1625,1057)  # Clicar para mudar de conta para Chapolin
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(1479,966)  # Clicar na Chapolin
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2341, 566)  # Ir para a ilha e clicar na aldeia 1
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2072,785)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(5)
+
+ pyautogui.moveTo(2194, 594)  # Ir para a ilha e clicar na aldeia 2
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2072,785)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2246, 687)  # Ir para a ilha e clicar na aldeia 3
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2072,785)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2374, 676)  # Ir para a ilha e clicar na aldeia 4
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2072,785)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2433, 719)  # Ir para a ilha e clicar na aldeia 5
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2072,785)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2516, 733)  # Ir para a ilha e clicar na aldeia 6
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2072,785)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(5)
+
+ pyautogui.moveTo(1625,1057)  # mudar para conta do Patati
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(1688,978)  # Clicar para conta do Patati
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2341, 566)  # Ir para a ilha e clicar na aldeia 1
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2072,785)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(5)
+
+ pyautogui.moveTo(2194, 594)  # Ir para a ilha e clicar na aldeia 2
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2072,785)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2246, 687)  # Ir para a ilha e clicar na aldeia 3
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2072,785)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2374, 676)  # Ir para a ilha e clicar na aldeia 4
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2072,785)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(5)
+
+ pyautogui.moveTo(1625, 1057)  # Ir para conta do Assassin
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(5)
+
+ pyautogui.moveTo(1881, 983)  # Clicar no Assassin
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(5)
+
+ pyautogui.moveTo(2356,554)  # Ir para a ilha e clicar na aldeia 1
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2064,712)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2688,300)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2200,579)  # Ir para a ilha e clicar na aldeia 2
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2064,712)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2688,300)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2251,667)  # Ir para a ilha e clicar na aldeia 3
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2064,712)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2688,300)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2377,657)  # Ir para a ilha e clicar na aldeia 4
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2064,712)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2688,300)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2432,702)  # Ir para a ilha e clicar na aldeia 5
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2064,712)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2671,305)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2512,712)  # Ir para a ilha e clicar na aldeia 6
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2064,712)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2679,308)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(1580,1055)  # Ir para conta do Gengis
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(300)
+
+
+
---
+++ Bot completo do Gengis.py
@@ -0,0 +1,191 @@
+
+import time
+import pyautogui
+
+pyautogui.PAUSE = 0.5
+
+# Ir para a conta do Gengis
+pyautogui.moveTo (1583,1061)
+pyautogui.mouseDown()
+pyautogui.mouseUp()
+time.sleep (3)
+
+# Ir para a ilha para atacar as aldeias barbaras
+pyautogui.moveTo (1410, 126)
+pyautogui.mouseDown()
+pyautogui.mouseUp()
+time.sleep (3)
+
+while True:
+ pyautogui.moveTo(2341,566)  # Ir para a ilha e clicar na aldeia 1
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2055, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2668,318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2194,594)  # Ir para a ilha e clicar na aldeia 2
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2055, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2246,687)  # Ir para a ilha e clicar na aldeia 3
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2055, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2374,676)  # Ir para a ilha e clicar na aldeia 4
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2055, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2433,719)  # Ir para a ilha e clicar na aldeia 5
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2055, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2516,733)  # Ir para a ilha e clicar na aldeia 6
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2055, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+ pyautogui.moveTo(2668, 318)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2394,124)  # Clicar para ir para cidade Gengis II
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2321, 193)  # Clicar em Gengis II
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(1478, 145)  # Clicar para mudar de ilha
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2433,489)  # Ir para a ilha e clicar na aldeia 1 da cidade II
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2066, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2671, 316)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2532,535)  # Ir para a ilha e clicar na aldeia 2 da cidade II
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2066, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2671, 316)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2565,644)  # Ir para a ilha e clicar na aldeia 3 da cidade II
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2066, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2671, 316)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2316,667)  # Ir para a ilha e clicar na aldeia 4 da cidade II
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2066, 821)  # clicar em 10 min
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2671, 316)  # Fechar a janela
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2394, 124)  # Clicar para ir para cidade Gengis
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(2328, 170)  # Clicar em Gengis
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(3)
+
+ pyautogui.moveTo(1478, 145)  # Clicar para mudar de ilha
+ pyautogui.mouseDown()
+ pyautogui.mouseUp()
+ time.sleep(560)
+
+
+
+
+
